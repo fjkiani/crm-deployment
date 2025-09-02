b0b3d6422db096aa4161cc90facda29e8df74575
Fahad Kiani <fjkiani@icloud.com>
2025-09-02 12:26:39 -0400

    SPA: add Human Inbox route/tab and bundle assets

---
+++ crm/public/frontend/assets/global-5f0b21a6.js
@@ -0,0 +1,2 @@
+import{ac as c,ah as s}from"./index-6bb6ee33.js";const i=c("crm-global",()=>{const e=s(),{$dialog:a,$socket:l}=e.appContext.config.globalProperties;let o=()=>{};function n(t){o=t}function r(t){o(t)}return{$dialog:a,$socket:l,makeCall:r,setMakeCall:n}});export{i as g};
+//# sourceMappingURL=global-5f0b21a6.js.map
